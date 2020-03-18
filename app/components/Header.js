de3fa37d982e92da9dbf6282e64f8b5b8178d559
Bouchebaba Amine <ABouchebaba@outlook.com>
2020-03-18 16:54:28 +0100

    Fixed MainHeader : item alignment and navigation

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,35 +1,48 @@
 import React, { useEffect } from "react";
-import { Text, View, Button, Image, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  Button,
+  Image,
+  TouchableOpacity,
+  ViewPagerAndroidComponent
+} from "react-native";
 import { useDispatch } from "react-redux";
 import { useNavigation } from "@react-navigation/native";
 
-function Header(props) {
-  const navigation = useNavigation();
+export function HeaderLeft(props) {
+  // const navigation = useNavigation();
   return (
-    <View style={style.container}>
+    <TouchableOpacity
+      style={style.left}
+      onPress={() => {
+        props.navigation.openDrawer();
+      }}
+    >
       <Image source={require("../../assets/hamburger.png")} />
+    </TouchableOpacity>
+  );
+}
 
-      <Text>Open News</Text>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate("SearchArticle");
-        }}
-      >
-        <Image source={require("../../assets/search.png")} />
-      </TouchableOpacity>
-    </View>
+export function HeaderRight(props) {
+  // const navigation = useNavigation();
+  return (
+    <TouchableOpacity
+      style={style.right}
+      onPress={() => {
+        props.navigation.navigate("Search");
+      }}
+    >
+      <Image source={require("../../assets/search.png")} />
+    </TouchableOpacity>
   );
 }
 
 const style = {
-  container: {
-    backgroundColor: "white",
-    elevation: 2,
-    padding: 15,
-    flex: 2,
-    flexDirection: "row",
-    alignItems: "flex-end",
-    justifyContent: "space-between"
+  left: {
+    paddingLeft: 20
+  },
+  right: {
+    paddingRight: 20
   }
 };
-export default Header;
--- app/navigation/Stack.js
+++ app/navigation/Stack.js
@@ -1,15 +1,30 @@
 import React from "react";
+import { Text, Button } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
-
 import Main from "./Main";
 import Search from "../screens/Search";
+import { HeaderLeft, HeaderRight, HeaderTitle } from "../components/Header";
+
+const MainHeader = props => <Text>Header</Text>;
 
 const Stack = createStackNavigator();
 
 function StackScreen() {
   return (
     <Stack.Navigator headerMode="screen">
-      <Stack.Screen name="Home" component={Main} />
+      <Stack.Screen
+        name="Home"
+        component={Main}
+        options={props => ({
+          headerRight: () => <HeaderRight {...props} />,
+          headerLeft: () => <HeaderLeft {...props} />,
+          headerTitle: "Open News",
+          headerTitleAlign: "center",
+          headerTitleStyle: {
+            fontWeight: "bold"
+          }
+        })}
+      />
       <Stack.Screen name="Search" component={Search} />
     </Stack.Navigator>
   );
