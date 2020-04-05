95d461bbc50b8abd9c2db40f191c482b74ee20c4
Bouchebaba Amine <ABouchebaba@outlook.com>
2020-04-05 11:20:50 +0100

    changed image place holder

--- app/helpers/index.js
+++ app/helpers/index.js
@@ -1,21 +1,22 @@
 import { BACKEND_IMAGE_DIR } from "react-native-dotenv";
 import { Dimensions } from "react-native";
 
-export const getImageSource = partial_link => {
+export const getImageSource = (partial_link) => {
   if (partial_link) return { uri: BACKEND_IMAGE_DIR + partial_link };
   // fallback image in assest
-  return require("../../assets/splash.png");
+  return { uri: "http://placehold.jp/320x240.png" };
+  // return require("../../assets/splash.png");
 };
 
 export const getDimensions = () => Dimensions.get("window");
 
-export const dateDifference = d => {
+export const dateDifference = (d) => {
   let current = new Date();
   let date = new Date(d.replace(" ", "T"));
   return timeAgoString(current - date);
 };
 
-const timeAgoString = milis => {
+const timeAgoString = (milis) => {
   let hours = Math.floor(milis / 1000 / 60 / 60);
   let days = Math.floor(hours / 24);
   if (days > 0) {
